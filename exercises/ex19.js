b024fef3442fc2828851cd1d63e91ea9139e5a57
kirakira <15catinthebox@gmail.com>
2026-01-30 12:04:57 -0800

    ex 19

--- exercises/ex19.js
+++ exercises/ex19.js
@@ -5,7 +5,8 @@ A game of chess is played on an 8 column by 8 row board. In the game of chess, a
 
 Chess Board Queen
 
-In JavaScript, we can represent a chessboard using an 8 by 8 array (8 arrays within an array). For this exercise, our chess board will have 2 queens, and nothing else. A 1 in the array represents a queen on the corresponding square, and a O in the array represents an unoccupied square.
+In JavaScript, we can represent a chessboard using an 8 by 8 array (8 arrays within an array). For this exercise, our chess board will have 2 queens, and nothing else.
+A 1 in the array represents a queen on the corresponding square, and a O in the array represents an unoccupied square.
 
 So the following chess board:
 
@@ -23,7 +24,8 @@ Would be represented in JavaScript like this:
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0]
 ]
-Our first challenge will be to write a function that generates a chess board like this. We will then write a function to detect weather or not the two queens are positioned so that they attack each other.
+Our first challenge will be to write a function that generates a chess board like this.
+We will then write a function to detect weather or not the two queens are positioned so that they attack each other.
 
 
 let whiteQueen = [0, 5];
@@ -65,3 +67,57 @@ Instruction
 Create a function generateBoard which will return a nested array representing the board, containing the location of two queens.
 Create a function called queenThreat that will indicate whether or not the two queens are positioned so that they attack each other.
 */
+
+
+
+
+const generateBoard = (whiteQueen, blackQueen) => {
+  const result = [];
+  for (let i = 0; i < 8; i++) {
+    result.push(Array(8).fill(0));
+  }
+
+  result[whiteQueen[0]][whiteQueen[1]] = 1
+  result[blackQueen[0]][blackQueen[1]] = 1
+
+  return result
+}
+
+const queenThreat = (board) => {
+  const positionOne = []
+  const positionTwo = []
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === 1) {
+        if (positionOne.length === 0) {
+          positionOne.push(i)
+          positionOne.push(j)
+        } else {
+          positionTwo.push(i)
+          positionTwo.push(j)
+        }
+      }
+    }
+  }
+
+  if (positionOne[0] === positionTwo[0]) return true
+  if (positionOne[1] === positionTwo[1]) return true
+  if ((Math.abs(positionOne[0] - positionTwo[0])) === Math.abs((positionOne[1] - positionTwo[1]))) return true
+
+  return false
+}
+
+// let whiteQueen = [0, 5]
+// let blackQueen = [5, 0]
+
+let whiteQueen = [0, 0];
+let blackQueen = [5, 7];
+
+
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
+
+
+module.exports = { generateBoard, queenThreat }
